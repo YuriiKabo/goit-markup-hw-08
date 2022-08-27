fb104dfeae4a2dd2fc0923f29b689587b7bb771c
YuriiKabo <yourrocksmirnov@gmail.com>
2022-08-27 13:50:41 +0300

    270822 1350

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector(".header__menu-open-button"),
     closeMenuBtn: document.querySelector(".mob-menu__close-button"),
     menu: document.querySelector(".mob-menu"),
+    body: document.querySelector("body"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -10,19 +11,20 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
 })(); */
 
 (() => {
-  const mobileMenu = document.querySelector(".js-menu-container");
-  const openMenuBtn = document.querySelector(".js-open-menu");
-  const closeMenuBtn = document.querySelector(".js-close-menu");
+  const mobileMenu = document.querySelector(".mob-menu");
+  const openMenuBtn = document.querySelector(".header__menu-open-button");
+  const closeMenuBtn = document.querySelector(".mob-menu__close-button");
 
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-    mobileMenu.classList.toggle("is-open");
+    mobileMenu.classList.toggle("is-hidden");
 
     const scrollLockMethod = !isMenuOpen
       ? "disableBodyScroll"
@@ -36,7 +38,7 @@
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
-    mobileMenu.classList.remove("is-open");
+    mobileMenu.classList.remove("is-hidden");
     openMenuBtn.setAttribute("aria-expanded", false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
