cdb98a711b3165e7329486bc6434e4902fc9cd35
YuriiKabo <yourrocksmirnov@gmail.com>
2022-08-27 21:29:43 +0300

    270822 2129

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -16,15 +16,15 @@
 })(); */
 
 (() => {
-  const mobileMenu = document.querySelector(".mob-menu");
-  const openMenuBtn = document.querySelector(".header__menu-open-button");
-  const closeMenuBtn = document.querySelector(".mob-menu__close-button");
+  const mobileMenu = document.querySelector(".js-menu-container");
+  const openMenuBtn = document.querySelector(".js-open-menu");
+  const closeMenuBtn = document.querySelector(".js-close-menu");
 
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-    mobileMenu.classList.toggle("is-hidden");
+    mobileMenu.classList.toggle("is-open");
 
     const scrollLockMethod = !isMenuOpen
       ? "disableBodyScroll"
@@ -38,7 +38,7 @@
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
-    mobileMenu.classList.remove("is-hidden");
+    mobileMenu.classList.remove("is-open");
     openMenuBtn.setAttribute("aria-expanded", false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
